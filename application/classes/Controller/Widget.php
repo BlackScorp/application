9585e92f89575ed64e6b3ad935752f7e9244233c
BlackScorp <witalimik@web.de>
2013-01-24 15:46:38 +0100

    disable widget on initial request

--- application/classes/Controller/Widget.php
+++ application/classes/Controller/Widget.php
@@ -5,6 +5,9 @@
 class Controller_Widget extends Controller_Index {
    
     public function before() {
+        //Controller not Exists as initial request
+        if($this->request->is_initial()) throw new HTTP_Exception_404;
+        
         $this->auto_render = FALSE;
         parent::before();
     }
